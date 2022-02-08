261af45498782684207482c4f428ba11cde7bf0a
Мирослав Атаманов <9atamanov1@mail.ru>
2022-02-08 09:12:31 +0700

    Добавлен проект

---
+++ WMS.Manager/WMS.Manager.WPF/MainWindow.xaml.cs
@@ -0,0 +1,9 @@
+﻿using MahApps.Metro.Controls;
+
+namespace WMS.Manager.WPF
+{
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow() => InitializeComponent();
+    }
+}
