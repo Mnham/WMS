0b75e4061588cf870bf55e003aaed3b8f766f242
Mnham <yurazamastsev@gmail.com>
2022-02-12 15:30:36 +0700

    Удалены проекты

--- WMS.ClassLibrary/WMS.ClassLibrary/Infrastructure/Models/IItemsModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace WMS.ClassLibrary.Infrastructure.Models
-{
-    public interface IItemsModel<TItemsModel> where TItemsModel : class
-    {
-        IReadOnlyList<TItemsModel> Items { get; set; }
-    }
-}
